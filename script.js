429747a46aac4db60c86b2a519f3ad306ad21f8c
Mel <melcornish@me.com>
2021-09-14 10:47:56 +0100

    update nav link & insta address

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ const nav = document.querySelector(".nav-list");
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--visible");
+
+  setTimeout(function () {
+    document.querySelector('.nav').style.display = 'none'
+  }, 5000)
+  setTimeout("location.reload(true);", 5000);
+
 });
